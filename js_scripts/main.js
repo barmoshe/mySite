1a2e78bb813590605d6f85ab0d50c9e8d55f51e8
barmoshe <1barmoshe1@gmail.com>
2021-09-13 20:40:42 +0300

    Update main.js

--- js_scripts/main.js
+++ js_scripts/main.js
@@ -22,18 +22,10 @@ function linkAction() {
     /*Remove menu mobile*/
     const navMenu = document.getElementById('nav-menu');
     navMenu.classList.remove('show');
-    const deviceType = () => {
-        const ua = navigator.userAgent;
-        if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-            return "tablet";
-        } else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
-            return "mobile";
-        }
-        return "desktop";
-    };
-    if (deviceType == "mobile") {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        // true for mobile device
         var homeIMG = document.getElementById('home_photo');
-        homeIMG.parentNode.removeChild(homeIMG);
+        homeIMG.remove();
 
     }
 
