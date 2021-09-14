c18e2694d2f415a5a08130111258969ee92283b2
barmoshe <1barmoshe1@gmail.com>
2021-09-14 18:39:57 +0300

    .

--- js_scripts/main.js
+++ js_scripts/main.js
@@ -26,12 +26,13 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 /*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
+sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 2000,
-    reset: true
+    duration: 1600,
+    reset: true,
 });
+
 
 /*SCROLL HOME*/
 sr.reveal('.home__title', {});
@@ -43,12 +44,24 @@ sr.reveal('.home__social-icon', { interval: 200 });
 sr.reveal('.about__img', {});
 sr.reveal('.about__subtitle', { delay: 400 });
 sr.reveal('.about__text', { delay: 400 });
-
+ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 1600,
+    reset: true,
+});
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {});
 sr.reveal('.skills__text', {});
 sr.reveal('.skills__data', { interval: 200 });
 sr.reveal('.skills__img', { delay: 600 });
+
+ScrollReveal({
+    origin: 'top',
+    distance: '65px',
+    duration: 1600,
+    reset: true,
+});
 
 /*SCROLL WORK*/
 sr.reveal('.work__staff', { interval: 200 });
