b8681de5239600a8337041b943b880cf48430bf1
barmoshe <1barmoshe1@gmail.com>
2021-09-15 14:11:59 +0300

    js added to reveal div on picturs

--- js_scripts/main.js
+++ js_scripts/main.js
@@ -69,7 +69,23 @@ sr.reveal('.work__staff', { interval: 200 });
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input', { interval: 200 });
 
-const form = document.getElementById('EmailForm')
-form.addEventListener('submit', (e) => {
-    alert('Thank you for reaching me, I will reply as soon as possible');
-})
+
+var textOverImages = document.getElementsByClassName("onClickTextOverImage");
+var previousTextOverImage;
+for (var i = 0; i < textOverImages.length; i++) {
+    textOverImages[i].onclick = function() {
+        var classes = this.classList;
+        if (classes.contains("show")) {
+            classes.remove("show");
+        } else {
+            if (previousTextOverImage != null)
+                previousTextOverImage.classList.remove("show");
+            previousTextOverImage = this;
+            classes.add("show");
+        }
+    }
+}
+
+function stopPropagation(event) {
+    event.stopPropagation();
+}
